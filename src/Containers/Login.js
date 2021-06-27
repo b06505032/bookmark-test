8e324b22f652608845949bf47a6301727529dedd
b06505032 <b06505032@ntu.edu.tw>
2021-06-27 09:19:52 +0800

    fix cookie

--- src/Containers/Login.js
+++ src/Containers/Login.js
@@ -25,7 +25,7 @@ const Login = ({setAccount, setSignIn, setUserCookie, setLoginCookie}) => {
                 case "success": {
                     const loginAccount = {name: input.name, password: input.password}
                     setAccount(loginAccount)
-                    setUserCookie('account', loginAccount, {path: '/', domain:'https://bookmark-group-manager.vercel.app', secure: true})
+                    setUserCookie('account', loginAccount, {path: '/'})
                     setLoginCookie('login', true, {path: '/'})
                     setSignIn(true)
                     break
